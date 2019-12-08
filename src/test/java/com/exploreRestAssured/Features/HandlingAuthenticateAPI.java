b980c8dde555f3dd08767f90f01140add722b18a
Sumit Kumar Chaudhary <sumitk440@gmail.com>
2019-12-08 18:10:46 +0530

    handling the authenticated api and sending the params & header

--- src/test/java/com/exploreRestAssured/Features/HandlingAuthenticateAPI.java
+++ src/test/java/com/exploreRestAssured/Features/HandlingAuthenticateAPI.java
@@ -13,25 +13,26 @@ import io.restassured.response.Response;
 
 public class HandlingAuthenticateAPI 
 {
-	{
-		//Storing the jSon response in the response reference variable 
-		 Response res= 		 
-		 
-		 given()
-		 //Expected / Fetch the data in jSon format
-		 .contentType(ContentType.JSON)
-		 .auth()  									 
-		 .basic("userName","Password")				//Pass the authorization
-		 .param("paramsKey1", "paramValue1")		//Passing the params key and value 
-		 .param("paramsKey N", "paramValue N")		
-		 .header("HeaderKey1", "Header Value 1")	//Passing the header value 
-		 .header("HeaderKey N", "Header Value N")
-		 .when().get("API URI");		// Get the data from the desire URL 
-		 
-		 
-		 System.out.println("Status Code is \t:\t"+res.statusCode());// Print the status code 
-		 System.out.println("Response is "+res.asString());// print the data in jSon Formate
-		
+	public static void main(String[] args) {
+		{
+			//Storing the jSon response in the response reference variable 
+			 Response res= 		 
+			 
+			 given()
+			 //Expected / Fetch the data in jSon format
+			 .contentType(ContentType.JSON)
+			 .auth()  									 
+			 .basic("userName","Password")				//Pass the authorization
+			 .param("paramsKey1", "paramValue1")		//Passing the params key and value 
+			 .param("paramsKey N", "paramValue N")		
+			 .header("HeaderKey1", "Header Value 1")	//Passing the header value 
+			 .header("HeaderKey N", "Header Value N")
+			 .when().get("API URI");		// Get the data from the desire URL 
+			 
+			 System.out.println("Status Code is \t:\t"+res.statusCode());// Print the status code 
+			 System.out.println("Response is "+res.asString());// print the data in jSon Formate
+			
+		}
 	}
 
 
